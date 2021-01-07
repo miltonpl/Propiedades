59d030de8c039961d8dbf43a96b3e6692293a2a9
Milton <miltonpalaguachi@gmail.com>
2021-01-06 23:13:15 -0500

    Initial Commit

---
+++ Propiedades/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Propiedades
+//
+//  Created by Milton Palaguachi on 1/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
